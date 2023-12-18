f326e2458d0cc5e2017db6fb1a6902ab37d1099e
a-dugonin <a_dugonin@mail.ru>
2023-12-18 20:35:31 +0300

    task_3: Тестирование функциональьности создания кораблей в main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from coordinate_point import PointCoordinates
 from human import Human
+from ships import Ship, WarShip, CargoShip
 
-
+# coordinate_point
 point_1 = PointCoordinates(x=1, y=1)
 print(point_1)
 point_2 = PointCoordinates(x=3, y=2)
@@ -11,6 +12,7 @@ print(point_3)
 print()
 print()
 
+# human
 human_1 = Human(name='Антон')
 human_1.set_age(age=35)
 human_1._Human__age = 18
@@ -18,6 +20,15 @@ human_2 = Human(name='Анна', age=18)
 print(human_1)
 print(human_2)
 print(f'Количество человек - {human_1.get_count()}')
+print()
+print()
 
-
-
+# ships
+cargo_ship = CargoShip(model='cargo_123')
+war_ship = WarShip(model='war_craft_ship_777', gun='пушка')
+print(cargo_ship)
+cargo_ship.load_ship()
+cargo_ship.unload_ship()
+print(war_ship)
+war_ship.attack()
+cargo_ship.walk_on_water()
